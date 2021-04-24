2842ed635012d0e369a3b57b02ed3ab645a0d12f
Sara-06 <83103957+Sara-06@users.noreply.github.com>
2021-04-24 08:02:18 +0500

    Update ASS147-1000-2426-serverless architecture google cloud platform.docx.java

--- ASS147-1000-2426-serverless architecture google cloud platform.docx.java
+++ ASS147-1000-2426-serverless architecture google cloud platform.docx.java
@@ -3,7 +3,6 @@ public class Inputfunctions {
   public static void main(String[] args) {
    Scanner readme = new Scanner(System.in);
    System.out.println("Enter a Number:");
-   //two variables to hold numbers
    double a1, a2, a3, a4, a5;
    a1 = readme.nextDouble();
    a2 = 20;
