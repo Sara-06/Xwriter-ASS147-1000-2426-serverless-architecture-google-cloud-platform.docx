88b6d7bd9018664ba5d7e2fbbac9e2f7c3f4af3b
Sara-06 <83103957+Sara-06@users.noreply.github.com>
2021-04-24 08:00:12 +0500

    Add files via upload

---
+++ ASS147-1000-2426-serverless architecture google cloud platform.docx.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Inputfunctions {
+  public static void main(String[] args) {
+   Scanner readme = new Scanner(System.in);
+   System.out.println("Enter a Number:");
+   //two variables to hold numbers
+   double a1, a2, a3, a4, a5;
+   a1 = readme.nextDouble();
+   a2 = 20;
+   a4= 50;
+   a3 = a1 / a2;
+   a5 = a3*a4;
+   if (a5 < 25) {
+  System.out.println(a5 + "Need Improvement");
+} else {
+  System.out.println(a5 + "Very Good.");
+}
+   
+  }
+}
